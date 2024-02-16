9fb1c99ffb9df49783c320c37ac7e9fab8ec7451
nova_44056 <senguy44056@gmail.com>
2024-02-16 12:21:29 -0600

    update config

--- config.ts
+++ config.ts
@@ -13,7 +13,7 @@ if (!domain) {
 export const httpPort: number = Number(process.env.HTTPPORT) || 80;
 export const httpsPort: number = Number(process.env.HTTPSPORT) || 443;
 export const publicCert: string = fs.readFileSync("./cert.pem", "utf-8");
-export const privateKey: string = fs.readFileSync("./key.pem", "utf-8");
+export const privateKey: string = fs.readFileSync("./decrypted_key.pem", "utf-8");
 export const sessionSecret: string = process.env.SESSION_SECRET as string;
 export const logFormat: string = process.env.LOGFORMAT || "dev";
 export const serverPort: number = Number(process.env.PORT) || 3000;
